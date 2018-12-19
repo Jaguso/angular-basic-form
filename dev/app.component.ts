a8554f95e63dcb105ec55874d4e2b4e2f7e6fcff
jaguso <jagutzo@gmail.com>
2018-12-19 17:39:59 -0600

    a

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,31 +3,39 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h3
-        (click)="onSelect()"
-        [class.clicked]="showDetail === true"
-        >{{contact.firstName}} {{contact.lastName}}</h3>
-        <input [(ngModel)]="contact.firstName" type="text">
-        <div>
-            <ul>
-                <li *ngIf="showDetail === true">
-                Email: {{contact.email}}
-                </li>
-                <li *ngIf="showDetail === false">
-                Address: {{contact.address}}
-                </li>
-            </ul>
-        </div>
+        <ul>
+            <li *ngFor="#contact of contacts"
+                (click)="onSelect()"
+                [class.clicked]="showDetail === true"
+            >
+            {{contact.firstName}} {{contact.lastName}}
+            </li>
+        </ul>
+
     `,
     styleUrls: ["../src/css/app.css"]
 })
 export class AppComponent {
-    public contact = {
-        firstName: "Hec",
-        lastName: "Herrera",
-        email: "hh@gmail.com",
-        address: "sodijv"
-    };
+    public contacts = [
+        {
+            firstName: "Marc",
+            lastName: "Herrera",
+            email: "hh@gmail.com",
+            address: "sodijv"
+        },
+        {
+            firstName: "David",
+            lastName: "Marquez",
+            email: "david@gmail.com",
+            address: "sodijv"
+        },
+        {
+            firstName: "Luke",
+            lastName: "Skywalker",
+            email: "luke@gmail.com",
+            address: "sodijv"
+        }
+    ];
 
     public showDetail = false;
 
